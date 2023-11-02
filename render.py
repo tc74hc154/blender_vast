f6a98d59863c46012ceb3c4637c8ef96bb40330c
hako <tetsuya20030000@gmail.com>
2023-11-02 16:02:02 +0900

    commit

---
+++ render.py
@@ -0,0 +1,33 @@
+import gradio as gr
+import os
+import subprocess
+
+# ファイルを受け取り、Blenderを開始
+def start_blender(file):
+    global subprocess_running
+    subprocess_running = True
+
+    # Pythonファイルのディレクトリを取得
+    script_dir = os.path.dirname(os.path.realpath(__file))
+
+    # Blenderコマンドを実行するためのコマンドリスト
+    command = [
+        os.path.join(script_dir, 'blender', 'blender'),  # Blender実行可能ファイルのパス
+        '-b',  # バッチモード
+        '-noaudio',  # オーディオを無効にする
+        '-P', os.path.join(script_dir, 'render.py')  # Pythonスクリプトのパス
+    ]
+
+    # サブプロセスを起動
+    subprocess.Popen(command)
+
+# Gradioのインターフェースを設定
+iface = gr.Interface(
+    fn=start_blender,  # ファイルアップロード時に呼び出される関数
+    inputs="file",     # ファイルのアップロードを許可
+    outputs="text",    # テキストの出力を設定
+    live=True           # リアルタイムでアップロード可能にする
+)
+
+# インターフェースを開始
+iface.launch()
