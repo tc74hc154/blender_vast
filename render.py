f3d73fc4523daa3aaac15d3d3694ad07d06f1e63
hako <tetsuya20030000@gmail.com>
2024-02-11 19:55:10 +0900

    uitest1

--- render.py
+++ render.py
@@ -1,34 +1,9 @@
 import gradio as gr
-import os
-import subprocess
 
-# ファイルを受け取り、Blenderを開始
-def start_blender(file):
-    global subprocess_running
-    subprocess_running = True
+def greet(name):
+    return "Hello " + name + "!"
 
-    # Pythonファイルのディレクトリを取得
-    script_dir = os.path.dirname(os.path.realpath(__file))
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
-    # Blenderコマンドを実行するためのコマンドリスト
-    command = [
-        os.path.join(script_dir, 'blender', 'blender'),  # Blender実行可能ファイルのパス
-        '-b',  # バッチモード
-        '-noaudio',  # オーディオを無効にする
-        '-P', os.path.join(script_dir, 'render.py')  # Pythonスクリプトのパス
-    ]
-
-    # サブプロセスを起動
-    subprocess.Popen(command)
-
-# Gradioのインターフェースを設定
-iface = gr.Interface(
-    fn=start_blender,  # ファイルアップロード時に呼び出される関数
-    inputs="file",     # ファイルのアップロードを許可
-    outputs="text",    # テキストの出力を設定
-    live=True,           # リアルタイムでアップロード可能にする
-    share=True
-)
-
-# インターフェースを開始
-iface.launch()
+if __name__ == "__main__":
+    demo.launch(show_api=False)
